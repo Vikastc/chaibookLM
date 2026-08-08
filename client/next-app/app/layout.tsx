a1e189960b45df6454f87ca2c1c3dfbc5e919cbe
Vikastc <vikastc99@gmail.com>
2026-08-08 12:08:52 +0530

    Add client (my-app, next-app) and server files

---
+++ client/next-app/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono, JetBrains_Mono, Figtree } from "next/font/google"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils";
+
+const figtreeHeading = Figtree({subsets:['latin'],variable:'--font-heading'});
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontSans.variable, "font-mono", jetbrainsMono.variable, figtreeHeading.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
