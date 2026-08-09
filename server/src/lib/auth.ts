a76e1b839852966cc8a0f6c2f3171939951f9439
Vikastc <vikastc99@gmail.com>
2026-08-09 21:59:33 +0530

    add theme and provider

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -4,7 +4,7 @@ import { prisma } from "./db.js";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: [process.env.CLIENT_URL ?? "http://localhost:4000"],
+  trustedOrigins: [process.env.CLIENT_URL ?? "http://localhost:3001"],
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
