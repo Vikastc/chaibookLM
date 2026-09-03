4cedf42c93de8ee191ba621f8133aff0bd3a90db
Vikastc <vikastc99@gmail.com>
2026-09-03 11:35:42 +0530

    change icon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from "next"
+
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import QueryProvider from "@/components/providers/query-provider"
 import { Toaster } from "@/components/ui/sonner"
+
+export const metadata: Metadata = {
+  title: "chaibook",
+}
 
 export default function RootLayout({
   children,
